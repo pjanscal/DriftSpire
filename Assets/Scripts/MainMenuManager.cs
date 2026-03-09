71b2641b5e84450c10c88279b27d6bf9512a6d23
ItzSenn <97126182@st.deltion.nl>
2026-03-09 18:31:03 +0100

    Zoom Time Fix

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -6,6 +6,7 @@ public class MainMenuManager : MonoBehaviour
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+        Time.timeScale = 1f;
     }
 
     public void QuitGame()
