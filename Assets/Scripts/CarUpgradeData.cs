7c1bc1b74bd14052699884ab76cc7eb5eec49fa3
justbenthe <benthevisser11@gmail.com>
2026-03-10 13:33:16 +0100

    customisation updates

--- Assets/Scripts/CarUpgradeData.cs
+++ Assets/Scripts/CarUpgradeData.cs
@@ -6,4 +6,7 @@ public class CarUpgradeData
     public int colorIndex;
     public bool headlightsOn;
     public int hornIndex;
+
+    public int wheelIndex;
+    public int lightIndex;
 }
--- Assets/Scripts/UpgradeManager.cs
+++ Assets/Scripts/UpgradeManager.cs
@@ -5,33 +5,68 @@ public class UpgradeManager : MonoBehaviour
     public CarController car;
     public CurrencyManager currency;
 
+    public int colorPrice = 100;
+    public int wheelPrice = 200;
+    public int hornPrice = 150;
+    public int lightPrice = 250;
+
     void Start()
     {
         car.upgradeData = SaveSystem.Load();
         car.ApplyUpgrades();
     }
 
-    public void SetColor(int index)
+    public void BuyColor(int index)
     {
-        if (!currency.CanAfford(100)) return;
+        if (!currency.CanAfford(colorPrice)) return;
 
-        currency.Spend(100);
+        currency.Spend(colorPrice);
 
         car.upgradeData.colorIndex = index;
         car.ApplyUpgrades();
+
         SaveSystem.Save(car.upgradeData);
     }
 
     public void ToggleHeadlights()
     {
         car.upgradeData.headlightsOn = !car.upgradeData.headlightsOn;
+
         car.ApplyUpgrades();
         SaveSystem.Save(car.upgradeData);
     }
 
-    public void SetHorn(int index)
+    public void BuyHorn(int index)
     {
+        if (!currency.CanAfford(hornPrice)) return;
+
+        currency.Spend(hornPrice);
+
         car.upgradeData.hornIndex = index;
+        SaveSystem.Save(car.upgradeData);
+    }
+
+    public void BuyWheels(int index)
+    {
+        if (!currency.CanAfford(wheelPrice)) return;
+
+        currency.Spend(wheelPrice);
+
+        car.upgradeData.wheelIndex = index;
+        car.ApplyUpgrades();
+
+        SaveSystem.Save(car.upgradeData);
+    }
+
+    public void BuyLights(int index)
+    {
+        if (!currency.CanAfford(lightPrice)) return;
+
+        currency.Spend(lightPrice);
+
+        car.upgradeData.lightIndex = index;
+        car.ApplyUpgrades();
+
         SaveSystem.Save(car.upgradeData);
     }
 }
